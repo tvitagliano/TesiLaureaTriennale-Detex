c9d7debe1f3fe2b3c0761ecb1d1e25f7e0276574
tvitagliano <teresavitagliano97@gmail.com>
2023-08-27 11:33:51 +0200

    fix test_my_function

--- test_my_function.py
+++ test_my_function.py
@@ -1,24 +1,24 @@
-import unittests
-from FunctionExampleTest import blob_example, lazy_class_example, long_method_example, spaghetti_code_ciclomatico_example
+import unittest
+from FunctionExampleTest import blob_example, LazyClassExample, long_method_example, spaghetti_code_ciclomatico_example
 
 
 
 class TestMyModule(unittest.TestCase):
     def test_spaghetti_code(self):
-        result = spaghetti_code_ciclomatico_example(2, 3)
-        self.assertEqual(result, 5)
+        result = spaghetti_code_ciclomatico_example()
+        self.assertEqual(result, 0)
 
     def test_blob(self):
-        result = blob_example(2, 3)
-        self.assertEqual(result, 5)
+        result = blob_example()
+        self.assertEqual(result, 0)
 
     def test_lazy_class(self):
-        result = lazy_class_example(2, 3)
-        self.assertEqual(result, 5)
+        result = LazyClassExample()
+        self.assertEqual(result, 0)
 
     def test_long_method(self):
-        result = long_method_example(2, 3)
-        self.assertEqual(result, 5)
+        result = long_method_example()
+        self.assertEqual(result, 0)
 
 if __name__ == '__main__':
     unittest.main()
